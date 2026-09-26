d01021d07998ee7bed6daa58f546f58bf87c5d7a
amaisuarez <145448002+amaisuarez@users.noreply.github.com>
2026-09-26 17:19:10 +0100

    Delete whale-vocalization-patterns/whale-vocalization-patterns directory

--- whale-vocalization-patterns/whale-vocalization-patterns/src/whalecodas/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Tools for analysing sperm whale coda structure (DSWP dataset)."""
-from .data import load_codas, filter_codas, rhythm_matrix, ici_matrix, features_for_click_count
-from .clustering import cluster_by_click_count, evaluate, seed_stability
-from .tempo import add_session, fit_tempo_mixture, ashman_d, session_bootstrap_k, tempo_scan, sorted_components
